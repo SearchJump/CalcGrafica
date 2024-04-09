b8a076f24ada0184e94441fa56a5ce0a698d730d
Nicolas Juan Medina <150536767+SearchJump@users.noreply.github.com>
2024-04-08 21:28:08 -0300

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _1erEjercicio
+{
+    public class Calculadora
+    {
+        /* int num1 = new int();
+        int num2 = new int();
+        public int suma = new int();
+        public int resta = new int();*/
+
+        public int Sumar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Restar(int num1,int num2)
+        {
+            return num1 - num2;
+            
+        }
+
+    }
+}
